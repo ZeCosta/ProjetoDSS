7560b8abd68fc4ea49799768cd2bc36061be39c6
111Bond <joaoprp111@gmail.com>
2020-12-20 23:15:55 +0000

    Preencher classes

--- Turmas3L-1.4/src/uminho/dss/turmas3l/business/Gestao/Palete.java
+++ Turmas3L-1.4/src/uminho/dss/turmas3l/business/Gestao/Palete.java
@@ -1,4 +1,26 @@
 package uminho.dss.turmas3l.business.Gestao;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class Palete {
+    private String idPalete;
+    private Collection<String> mats;
+    private double peso;
+
+    public Palete(String id, double peso){
+        this.idPalete = id;
+        this.mats = null;
+        this.peso = peso;
+    }
+
+    public Palete(String id, Collection<String> mats, double peso) {
+        this.idPalete = id;
+        this.mats = new ArrayList<>();
+        this.mats.addAll(mats);
+        this.peso = peso;
+    }
+
+    public String getId(){
+        return this.idPalete;
+    }
 }
--- Turmas3L-1.4/src/uminho/dss/turmas3l/business/Gestao/Prateleira.java
+++ Turmas3L-1.4/src/uminho/dss/turmas3l/business/Gestao/Prateleira.java
@@ -1,4 +1,25 @@
 package uminho.dss.turmas3l.business.Gestao;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class Prateleira {
+    private String idPrat;
+    private double pesoMax;
+    private int paletesMax;
+    private Collection<String> paletes;
+
+    public Prateleira(String id, double peso, int nPaletes){
+        idPrat = id;
+        pesoMax = peso;
+        paletesMax = nPaletes;
+        paletes = new ArrayList<>();
+    }
+
+    public Prateleira(String id, double peso, int nPaletes, Collection<String> ids){
+        idPrat = id;
+        pesoMax = peso;
+        paletesMax = nPaletes;
+        paletes = new ArrayList<>();
+        paletes.addAll(ids);
+    }
 }
