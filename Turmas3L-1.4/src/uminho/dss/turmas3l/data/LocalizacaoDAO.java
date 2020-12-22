9acb00b9334f49ea88c00fba532b8583f399ee54
111Bond <joaoprp111@gmail.com>
2020-12-22 16:29:07 +0000

    LocalizacaoDAO

---
+++ Turmas3L-1.4/src/uminho/dss/turmas3l/data/LocalizacaoDAO.java
@@ -0,0 +1,251 @@
+package uminho.dss.turmas3l.data;
+
+import uminho.dss.turmas3l.business.Gestao.MateriaPrima;
+import uminho.dss.turmas3l.business.Gestao.Palete;
+import uminho.dss.turmas3l.business.Localizacao;
+
+import java.sql.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class LocalizacaoDAO implements Map<String, Localizacao> {
+    private static LocalizacaoDAO singleton = null;
+
+    private LocalizacaoDAO(){
+        try (Connection conn = DriverManager.getConnection(DAOConfig.URL, DAOConfig.USERNAME, DAOConfig.PASSWORD);
+             Statement stm = conn.createStatement()) {
+            String sql = "CREATE TABLE IF NOT EXISTS localizacao (" +
+                    "id varchar(10) NOT NULL PRIMARY KEY," +
+                    "l_local varchar(20) NOT NULL)";
+            stm.executeUpdate(sql);
+        } catch (SQLException e) {
+            // Erro a criar tabela...
+            e.printStackTrace();
+            throw new NullPointerException(e.getMessage());
+        }
+    }
+
+    /**
+     * Implementação do padrão Singleton
+     *
+     * @return devolve a instância única desta classe
+     */
+    public static LocalizacaoDAO getInstance() {
+        if (LocalizacaoDAO.singleton == null) {
+            LocalizacaoDAO.singleton = new LocalizacaoDAO();
+        }
+        return LocalizacaoDAO.singleton;
+    }
+
+    /**
+     * @return número de localizações na base de dados
+     */
+    @Override
+    public int size() {
+        int i = 0;
+        try (Connection conn = DriverManager.getConnection(DAOConfig.URL, DAOConfig.USERNAME, DAOConfig.PASSWORD);
+             Statement stm = conn.createStatement();
+             ResultSet rs = stm.executeQuery("SELECT count(*) FROM localizacao")) {
+            if(rs.next()) {
+                i = rs.getInt(1);
+            }
+        }
+        catch (Exception e) {
+            // Erro a criar tabela...
+            e.printStackTrace();
+            throw new NullPointerException(e.getMessage());
+        }
+        return i;
+    }
+
+    /**
+     * Método que verifica se existem localizações
+     *
+     * @return true se existirem 0 localizações
+     */
+    @Override
+    public boolean isEmpty() {
+        return this.size() == 0;
+    }
+
+    /**
+     * Método que verifica se um local(id da localizacao) existe na base de dados
+     *
+     * @param key id da localizacao
+     * @return true se existe
+     * @throws NullPointerException Em caso de erro - deveriam ser criadas exepções do projecto
+     */
+    @Override
+    public boolean containsKey(Object key) {
+        boolean r;
+        try (Connection conn = DriverManager.getConnection(DAOConfig.URL, DAOConfig.USERNAME, DAOConfig.PASSWORD);
+             Statement stm = conn.createStatement();
+             ResultSet rs =
+                     stm.executeQuery("SELECT local FROM palete WHERE local='"+key.toString()+"'")) {
+            r = rs.next();
+        } catch (SQLException e) {
+            // Database error!
+            e.printStackTrace();
+            throw new NullPointerException(e.getMessage());
+        }
+        return r;
+    }
+
+    /**
+     * Verifica se uma localizacao existe na base de dados
+     *
+     * Esta implementação é provisória. Devia testar o objecto e não apenas a chave.
+     *
+     * @param value ...
+     * @return ...
+     * @throws NullPointerException Em caso de erro - deveriam ser criadas exepções do projecto
+     */
+    @Override
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    /**
+     * Obter uma localizacao, dado o seu id (local)
+     *
+     * @param key local
+     * @return a localizacao caso exista (null noutro caso)
+     * @throws NullPointerException Em caso de erro - deveriam ser criadas exepções do projecto
+     */
+    @Override
+    public Localizacao get(Object key) {
+        Localizacao l = null;
+        try (Connection conn = DriverManager.getConnection(DAOConfig.URL, DAOConfig.USERNAME, DAOConfig.PASSWORD);
+             Statement stm = conn.createStatement();
+             ResultSet rs = stm.executeQuery("SELECT * FROM localizacao WHERE local='"+key.toString()+"'")) {
+            if (rs.next()) {  // A chave existe na tabela
+                // Reconstruir a Localizacao
+                String local = rs.getString("local");
+                l = new Localizacao(local);
+            }
+        } catch (SQLException e) {
+            // Database error!
+            e.printStackTrace();
+            throw new NullPointerException(e.getMessage());
+        }
+        return l;
+    }
+
+    /**
+     * @param key o local
+     * @param l a localizacao
+     * @return para já retorna sempre null (deverá devolver o valor existente, caso exista um)
+     * @throws NullPointerException Em caso de erro - deveriam ser criadas exepções do projecto
+     */
+    @Override
+    public Localizacao put(String key, Localizacao l) {
+        Localizacao res = null;
+        try (Connection conn = DriverManager.getConnection(DAOConfig.URL, DAOConfig.USERNAME, DAOConfig.PASSWORD);
+             Statement stm = conn.createStatement()) {
+
+            // adicionar localizacao se nao existe
+            stm.executeUpdate(
+                    "INSERT IGNORE INTO localizacao " +
+                            "VALUES ('"+ l.getLocal()+ "')");
+
+        } catch (SQLException e) {
+            // Database error!
+            e.printStackTrace();
+            throw new NullPointerException(e.getMessage());
+        }
+        return res;
+    }
+
+    /**
+     * Remover uma localizacao, dado o seu local
+     *
+     * NOTA: Não estamos a apagar a localizacao, mas estamos a apagar a materia...
+     *
+     * @param key local da localizacao a remover
+     * @return a localizacao removida
+     * @throws NullPointerException Em caso de erro - deveriam ser criadas exepções do projecto
+     */
+    @Override
+    public Localizacao remove(Object key) {
+        Localizacao l = this.get(key);
+        try (Connection conn = DriverManager.getConnection(DAOConfig.URL, DAOConfig.USERNAME, DAOConfig.PASSWORD);
+             Statement stm = conn.createStatement()) {
+
+            // apagar a localizacao
+            stm.executeUpdate("DELETE FROM localizacao WHERE local='"+l.getLocal()+"'");
+        } catch (Exception e) {
+            // Database error!
+            e.printStackTrace();
+            throw new NullPointerException(e.getMessage());
+        }
+        return l;
+    }
+
+    /**
+     * Adicionar um conjunto de localizacões à base de dados
+     *
+     * @param ls as localizações a adicionar
+     * @throws NullPointerException Em caso de erro - deveriam ser criadas exepções do projecto
+     */
+    @Override
+    public void putAll(Map<? extends String, ? extends Localizacao> ls) {
+        for(Localizacao l : ls.values()) {
+            this.put(l.getLocal(), l);
+        }
+    }
+
+    /**
+     * Apagar todas as localizações -> não achamos necessaria a implementação
+     *
+     * @throws NullPointerException Em caso de erro - deveriam ser criadas exepções do projecto
+     */
+    @Override
+    public void clear() {
+    }
+
+    /**
+     * NÃO IMPLEMENTADO!
+     * @return ainda nada!
+     */
+    @Override
+    public Set<String> keySet() {
+        throw new NullPointerException("Not implemented!");
+    }
+
+    /**
+     * @return Todas as localizações da base de dados
+     */
+    @Override
+    public Collection<Localizacao> values() {
+        Collection<Localizacao> res = new HashSet<>();
+        try (Connection conn = DriverManager.getConnection(DAOConfig.URL, DAOConfig.USERNAME, DAOConfig.PASSWORD);
+             Statement stm = conn.createStatement();
+             ResultSet rs = stm.executeQuery("SELECT local FROM localizacao")) { // ResultSet com os ids de todas as localizações
+            while (rs.next()) {
+                String idt = rs.getString("local"); // Obtemos um id da localização (local)
+                Localizacao l = this.get(idt);                    // Utilizamos o get para construir as localizações uma a uma
+                res.add(l);                                 // Adiciona a localização ao resultado.
+            }
+        } catch (Exception e) {
+            // Database error!
+            e.printStackTrace();
+            throw new NullPointerException(e.getMessage());
+        }
+        return res;
+    }
+
+    /**
+     * NÃO IMPLEMENTADO!
+     * @return ainda nada!
+     */
+    @Override
+    public Set<Entry<String, Localizacao>> entrySet() {
+        throw new NullPointerException("public Set<Map.Entry<String,Palete>> entrySet() not implemented!");
+    }
+
+
+
+
+}
